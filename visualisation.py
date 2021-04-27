845cdd492a16d8ba5aca6ac0b97fcf47e68f4080
Sytze Simonse <sytze-simonse@tutanota.com>
2021-04-27 12:33:57 +0200

    Creating function for displaying and storing segmentation inferences

---
+++ visualisation.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+from pathlib import Path
+import torch
+
+def show_training_results(images_folder: str, output_folder: str, threshold: float = 0.2, alpha_val: float = 0.4, palette: str = 'RdYlBu'):
+    # Creating paths
+    images_folder_path = Path(images_folder)
+    output_folder_path = Path(output_folder)
+    
+    # Getting list of images
+    images = os.listdir(images_folder)
+
+    for image in images:
+        # Creating image path
+        img_path = images_folder_path / image
+
+        # Reading image
+        img = cv2.imread(img_path)
+
+        # Setting channels in right order
+        img = img.transpose(2,0,1)
+
+        # Reshaping (opposite of ravel)
+        img = img.reshape(1, 3, 512, 512)
+
+        # Displaying image
+        plt.imshow(img[0,...].transpose(1,2,0))
+    
+        # Making prediction on image
+        with torch.no_grad():
+            prediction = model(torch.from_numpy(img).type(torch.cuda.FloatTensor)/255)
+
+        # Getting prediction result
+        result = prediction.cpu().detach().numpy()[0][0] > threshold
+        
+        # Overlaying prediction result
+        plt.imshow(
+            result, 
+            alpha=alpha_val, 
+            cmp=palette)
+
+        # Saving PNG of result in inferences folder
+        plt.savefig(output_folder_path / img)
+
+        # Displaying
+        plt.show()
