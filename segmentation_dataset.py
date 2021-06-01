1e5eb69dcd3d5a32d3196e31218a5839826544dc
Sytze Simonse <sytze-simonse@tutanota.com>
2021-06-01 13:02:44 +0200

    Only activating random crop for data aug

--- segmentation_dataset.py
+++ segmentation_dataset.py
@@ -199,8 +199,8 @@ class SegmentationDataset(VisionDataset):
                 sample = transform(sample, transformation_functions = [
                   #random_rotation,
                   #random_brightness
-                  random_contrast
-                  #random_crop                                            
+                  #random_contrast
+                  random_crop                                            
                 ])
             
             # Transforming
