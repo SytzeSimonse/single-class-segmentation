de636b3297c972b0d24dd4fcfe3f30d8425fad51
Sytze Simonse <sytze-simonse@tutanota.com>
2021-06-01 09:18:33 +0200

    Only activating contrast data aug

--- segmentation_dataset.py
+++ segmentation_dataset.py
@@ -198,8 +198,8 @@ class SegmentationDataset(VisionDataset):
             if self.data_augmentation and self.subset == "Train":
                 sample = transform(sample, transformation_functions = [
                   #random_rotation,
-                  random_brightness
-                  #random_contrast,
+                  #random_brightness
+                  random_contrast
                   #random_crop                                            
                 ])
             
